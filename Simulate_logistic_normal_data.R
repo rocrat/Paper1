f968f7e5fe8f28d1d4ba4cd5857091f83262819b
rocrat <dlaroche@email.arizona.edu>
2017-03-29 14:55:39 -0700

    Added script for simulating logistic normal data

---
+++ Simulate_logistic_normal_data.R
@@ -0,0 +1,11 @@
+###############################################################################
+###############################################################################
+## Simulate logistic normal data.  As per Dean's recommendation I start with 
+## simulating multivariate log-normal data as a basis and then imposing the 
+## sum constraint through the closure operation
+###############################################################################
+###############################################################################
+
+
+#Start with low dimension composition D=3
+
